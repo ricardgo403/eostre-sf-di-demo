3ac4b7db87094bb8b3e12aa8628f423e93e595f6
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-12 18:03:05 -0500

    Feat: PropertyInjectedController with Spring annotation Test class. Bootstraping what Spring does behind scenes

---
+++ src/test/java/com/example/ricardgo403/eostresfdidemo/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.ricardgo403.eostresfdidemo.controllers;
+
+import com.example.ricardgo403.eostresfdidemo.services.impl.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+    PropertyInjectedController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new PropertyInjectedController();
+        controller.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+        assertEquals("Hello, Sir @ricardgo403", controller.getGreeting());
+    }
+}
