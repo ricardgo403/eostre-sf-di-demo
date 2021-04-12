909ac8c6f792be7215433febe1d3fb2da7cdbd11
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-12 13:50:44 -0500

    Initial commit

---
+++ src/test/java/com/example/ricardgo403/eostresfdidemo/DependencyInjectionDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ricardgo403.eostresfdidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
