98910b9928fa4afece60898989ca5c041eef058d
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-12 18:04:13 -0500

    Feat: Bootstraping what Spring does behind scenes and seeing/using its context.

--- src/main/java/com/example/ricardgo403/eostresfdidemo/DependencyInjectionDemoProjectApplication.java
+++ src/main/java/com/example/ricardgo403/eostresfdidemo/DependencyInjectionDemoProjectApplication.java
@@ -1,13 +1,32 @@
 package com.example.ricardgo403.eostresfdidemo;
 
+import com.example.ricardgo403.eostresfdidemo.controllers.ConstructorInjectedController;
+import com.example.ricardgo403.eostresfdidemo.controllers.MyController;
+import com.example.ricardgo403.eostresfdidemo.controllers.PropertyInjectedController;
+import com.example.ricardgo403.eostresfdidemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionDemoProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionDemoProjectApplication.class, args);
+//		SpringApplication.run(DependencyInjectionDemoProjectApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoProjectApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println("myController ctx greeting: " + myController.getGreeting());
+		System.out.println("------Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println("PropertyInjectedController ctx greeting: " + propertyInjectedController.getGreeting());
+
+		System.out.println("------Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println("SetterInjectedController ctx greeting: " + setterInjectedController.getGreeting());
+
+		System.out.println("------Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println("ConstructorInjectedController ctx greeting: " + constructorInjectedController.getGreeting());
 	}
 
 }
