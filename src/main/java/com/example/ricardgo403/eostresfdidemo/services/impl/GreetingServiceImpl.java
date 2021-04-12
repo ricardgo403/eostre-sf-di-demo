0ceac3c3c78045346a74301d5c5bf8ef3b13df7a
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-12 18:01:29 -0500

    Feat: GreetingService Interface implemented for bootstrapping it in the tests

---
+++ src/main/java/com/example/ricardgo403/eostresfdidemo/services/impl/GreetingServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.ricardgo403.eostresfdidemo.services.impl;
+
+import com.example.ricardgo403.eostresfdidemo.services.IGreetingService;
+
+public class GreetingServiceImpl implements IGreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello, Sir @ricardgo403";
+    }
+}
