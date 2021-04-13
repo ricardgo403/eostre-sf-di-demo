fa6d6d7f38751a2f5d0e2580ef4075f1b31c18f5
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-13 10:48:52 -0500

    Feat: Qualifier for setter service impl.

--- src/main/java/com/example/ricardgo403/eostresfdidemo/controllers/SetterInjectedController.java
+++ src/main/java/com/example/ricardgo403/eostresfdidemo/controllers/SetterInjectedController.java
@@ -2,12 +2,14 @@ package com.example.ricardgo403.eostresfdidemo.controllers;
 
 import com.example.ricardgo403.eostresfdidemo.services.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
     private IGreetingService greetingService;
 
+    @Qualifier("setterGreetingServiceImpl")
     @Autowired
     public void setGreetingService(IGreetingService greetingService) {
         this.greetingService = greetingService;
---
+++ src/main/java/com/example/ricardgo403/eostresfdidemo/services/impl/SetterGreetingServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.ricardgo403.eostresfdidemo.services.impl;
+
+import com.example.ricardgo403.eostresfdidemo.services.IGreetingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingServiceImpl implements IGreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello, Sir @ricardgo403! - Setter";
+    }
+}
