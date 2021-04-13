039f63b799e7967ad887ca6fb4777d75aad6458d
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-13 12:27:14 -0500

    Feat: Default Profile Annotation, change made in properties to test if the default profile was applied.

--- src/main/java/com/example/ricardgo403/eostresfdidemo/services/impl/I18nSpanishGreetingServiceImpl.java
+++ src/main/java/com/example/ricardgo403/eostresfdidemo/services/impl/I18nSpanishGreetingServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.ricardgo403.eostresfdidemo.services.IGreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES-MX")
+@Profile({"ES-MX", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingServiceImpl implements IGreetingService {
     @Override
